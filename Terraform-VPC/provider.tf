204d09d2285049769cc0cb3e7e515cc2bbbf0c48
Emmanuel Mortoo <emortoo@yahoo.com>
2023-10-31 22:59:07 -0400

    commented out s3 info

--- Terraform-VPC/provider.tf
+++ Terraform-VPC/provider.tf
@@ -6,11 +6,11 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket = "emma-terraform-remote-backend-s3"
-    key    = "dev/terraform.tfstate"
-    region = "us-east-1"
-  }
+  //backend "s3" {
+   // bucket = "emma-terraform-remote-backend-s3"
+   // key    = "dev/terraform.tfstate"
+   // region = "us-east-1"
+ // }
 }
 
 provider "aws" {
